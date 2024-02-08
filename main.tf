029f55e313730f19c680727082b7ba877387d275
basava406 <158306734+basava406@users.noreply.github.com>
2024-02-08 15:06:04 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,107 @@
+provider "azurerm" {
+  features {}
+
+subscription_id = "d057dd37-e149-4466-9795-1b5ceeab9b3d"
+client_id = "a5b1e29b-b440-4b0c-8915-a1353abad00a"
+client_secret = "~._8Q~jQdIb63CDrDBDHEYxxh4MW_YGCeSxZCciF"
+tenant_id = "76c5d6a3-7d9b-4111-9805-12d9eb43d4ab"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.allowed_ssh_ip_address
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = "${var.vm_count}"
+  name                = "${var.prefix}-nic-${count.index}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip[count.index].id
+  }
+}
+
+resource "azurerm_public_ip" "pubip" {
+  count               = "${var.vm_count}"
+  name                = "${var.prefix}-public-ip-${count.index}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count                 = "${var.vm_count}"
+  name                  = "${var.prefix}-vm-${count.index}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  size                  = "Standard_B1s"
+  admin_username        = var.vm_admin_username
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+
+  admin_ssh_key {
+    username   = var.vm_admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+
+# ... (existing code)
+
+# ... (existing code)
+
+
+
+
+output "public_ip_address_generated"{
+  value = azurerm_public_ip.pubip
+}
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vm_admin_username" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
+}
+
+variable "allowed_ssh_ip_address" {
+  type = string
+}
+
+variable "vm_count" {
+  type = number
+}
+
+variable "prefix" {
+  type = string
+}
+
